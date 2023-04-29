c1b1f964cc55b5fc5f55c1e9250873e7f1872842
= <=>
2023-04-29 23:45:19 +0300

    feat: add 5 rows in virtual keyboard HTML structure (Sat, Apr 29, 2023 11:45:19 PM)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import './index.scss';
 
 const BODY = document.body;
-const TITLE = 'Виртуальная клавиатура';
-const H1 = document.createElement('h1');
+const TITLE = 'RSS Виртуальная клавиатура';
+const [H1, KEYBOARD_ROOT] = [document.createElement('h1'), document.createElement('div')];
+const KEYBOARD_ROWS = 5;
 
 H1.textContent = TITLE;
-BODY.appendChild(H1);
+BODY.append(H1, KEYBOARD_ROOT);
 BODY.classList.add('body');
 H1.classList.add('h1');
+KEYBOARD_ROOT.classList.add('keyboard-root');
+
+for (let i = 0; i < KEYBOARD_ROWS; i += 1) {
+  const KEYBOARD_ROW = document.createElement('div');
+  KEYBOARD_ROW.classList.add('keyboard-row', 'row');
+  KEYBOARD_ROOT.appendChild(KEYBOARD_ROW);
+}
