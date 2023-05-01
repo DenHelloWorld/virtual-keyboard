3152e4473e4a0b0e784f8512e81c0fbbee4e4eb0
= <=>
2023-05-01 21:07:51 +0300

    feat: add textarea (Mon, May 01, 2023 09:07:51 PM)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const KEYBOARD_ROWS = 5;
 const KEY_MAP = [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
   ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
   ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
-  ['LeftShift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'RightShift'],
+  ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
   ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl']];
 
 H1.textContent = TITLE;
@@ -29,3 +29,39 @@ for (let i = 0; i < KEYBOARD_ROWS; i += 1) {
   }
   KEYBOARD_ROOT.appendChild(KEYBOARD_ROW);
 }
+
+const TEXTAREA = document.createElement('textarea');
+BODY.appendChild(TEXTAREA);
+
+const KEY_BUTTONS = document.querySelectorAll('.keyboard-key');
+let isCapsLockOn = false;
+KEY_BUTTONS.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const key = button.textContent;
+    if (key === 'Backspace') {
+      TEXTAREA.value = TEXTAREA.value.slice(0, -1);
+    } else if (key === 'Space') {
+      TEXTAREA.value += ' ';
+    } else if (key === 'CapsLock') {
+      // переключение флага isCapsLockOn при нажатии на CapsLock
+      isCapsLockOn = !isCapsLockOn;
+      // изменение стиля кнопки CapsLock при переключении
+      button.classList.toggle('active', isCapsLockOn);
+    } else if (key === 'Shift') {
+      // изменение стиля кнопки Shift при нажатии
+      button.classList.add('active');
+    } else {
+      // обработка всех остальных кнопок
+      const isUpperCase = isCapsLockOn || event.getModifierState('Shift');
+      const char = isUpperCase ? key.toUpperCase() : key.toLowerCase();
+      TEXTAREA.value += char;
+    }
+  });
+
+  // удаление стиля 'active' при отпускании кнопки Shift
+  button.addEventListener('mouseup', () => {
+    if (button.textContent === 'Shift') {
+      button.classList.remove('active');
+    }
+  });
+});
